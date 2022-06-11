7c3c631cf327475827d37a0c641bedb4c45ff72e
Raul Puente <raul@rapulink.com>
2022-06-11 09:50:26 +0200

    Warning solved and added import on train.py

--- torch_srgan/datasets/datasets.py
+++ torch_srgan/datasets/datasets.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 
 import torch
+import torchvision.transforms
 from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
@@ -18,6 +19,7 @@ std = np.array([0.229, 0.224, 0.225])
 
 def denormalize(tensors):
     """ Denormalizes image tensors using mean and std """
+
     for c in range(3):
         tensors[:, c].mul_(std[c]).add_(mean[c])
     return torch.clamp(tensors, 0, 255)
@@ -29,14 +31,15 @@ class ImageDataset(Dataset):
         # Transforms for low resolution images and high resolution images
         self.lr_transform = transforms.Compose(
             [
-                transforms.Resize((hr_height // 4, hr_height // 4), Image.BICUBIC),
+
+                transforms.Resize((hr_height // 4, hr_height // 4), transforms.InterpolationMode.BICUBIC),
                 transforms.ToTensor(),
                 transforms.Normalize(mean, std),
             ]
         )
         self.hr_transform = transforms.Compose(
             [
-                transforms.Resize((hr_height, hr_height), Image.BICUBIC),
+                transforms.Resize((hr_height, hr_height), transforms.InterpolationMode.BICUBIC),
                 transforms.ToTensor(),
                 transforms.Normalize(mean, std),
             ]
